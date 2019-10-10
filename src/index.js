d280bf84b4bc8a3d1d476ecace01a7e35d4a7010
DeAdFrOnt <semenukmaks@gmail.com>
2019-10-11 01:45:13 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  for (i = str.length; i >= 0; i--){
+    for (j = 0; j < bracketsConfig.length; j++){
+      str = str.replace(bracketsConfig[j].join(''), '');
+    }
+  }
+  return !str;
 }
